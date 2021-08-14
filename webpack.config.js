aa2ab55c650a74dd2918c18e362553bc376c841f
cjshinningg <784319417@qq.com>
2021-08-14 13:09:34 +0800

    3-5 使用plugins让打包更便捷

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -36,6 +38,12 @@ module.exports = {
       ]
     }]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')

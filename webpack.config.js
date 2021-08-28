20bf14838dd29f08442d17d7ccd949ded2d3aea7
cjshinningg <784319417@qq.com>
2021-08-28 18:02:30 +0800

    4-1 Tree Shaking概念详解

--- webpack.config.js
+++ webpack.config.js
@@ -67,6 +67,9 @@ module.exports = {
     new CleanWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin()
   ],
+  optimization: {
+    usedExports: true
+  },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
